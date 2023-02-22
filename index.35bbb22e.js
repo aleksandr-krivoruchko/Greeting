06ebef9fe5b7f7095bba8cb642efdfc701c28bbb
aleksandr-krivoruchko <87823758+aleksandr-krivoruchko@users.noreply.github.com>
2023-02-22 11:36:42 +0000

    Deploying to gh-pages from @ aleksandr-krivoruchko/Greeting@0c46bf3c0e8a928f27f683ec69a3e463d27fa98c 🚀

---
+++ index.35bbb22e.js
@@ -0,0 +1,2 @@
+document.querySelector(".cta").addEventListener("click",(function(){document.querySelector(".sun").classList.toggle("is-hidden"),document.querySelector(".face").classList.toggle("is-hidden"),document.querySelector(".me").classList.toggle("is-hidden"),document.querySelector(".heart").classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.35bbb22e.js.map
