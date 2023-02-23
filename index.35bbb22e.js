a6f64d66c50f8844cf73a530e5d668aba2f4fb6f
aleksandr-krivoruchko <87823758+aleksandr-krivoruchko@users.noreply.github.com>
2023-02-23 12:43:08 +0000

    Deploying to gh-pages from @ aleksandr-krivoruchko/Greeting@340947a1b27b6c59894d898300d25e630bc844ba 🚀

--- index.35bbb22e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".cta").addEventListener("click",(function(){document.querySelector(".sun").classList.toggle("is-hidden"),document.querySelector(".face").classList.toggle("is-hidden"),document.querySelector(".me").classList.toggle("is-hidden"),document.querySelector(".heart").classList.toggle("is-hidden")}));
-//# sourceMappingURL=index.35bbb22e.js.map
---
+++ index.a8089a84.js
@@ -0,0 +1,2 @@
+document.querySelector(".cta").addEventListener("click",(function(){document.querySelector(".sun").classList.toggle("is-hidden"),document.querySelector(".face").classList.toggle("is-hidden"),document.querySelector(".cat").classList.toggle("is-hidden"),document.querySelector(".me").classList.toggle("is-hidden"),document.querySelector(".heart").classList.toggle("is-hidden"),document.querySelector(".phrase").classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.a8089a84.js.map
