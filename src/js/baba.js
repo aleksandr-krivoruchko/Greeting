340947a1b27b6c59894d898300d25e630bc844ba
AlexKriv <krivouzis1985@gmail.com>
2023-02-23 14:42:37 +0200

    add cat and phrase

--- src/js/baba.js
+++ src/js/baba.js
@@ -1,6 +1,8 @@
 document.querySelector('.cta').addEventListener('click', () => {
   document.querySelector('.sun').classList.toggle('is-hidden');
   document.querySelector('.face').classList.toggle('is-hidden');
+  document.querySelector('.cat').classList.toggle('is-hidden');
   document.querySelector('.me').classList.toggle('is-hidden');
   document.querySelector('.heart').classList.toggle('is-hidden');
+  document.querySelector('.phrase').classList.toggle('is-hidden');
 });
