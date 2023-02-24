b8645335be40494f946f3d013b553f0c23dcbbb6
aleksandr-krivoruchko <87823758+aleksandr-krivoruchko@users.noreply.github.com>
2023-02-24 12:04:32 +0000

    Deploying to gh-pages from @ aleksandr-krivoruchko/Greeting@80ea6b1b560d84b197b147cfda144e69a120b895 🚀

---
+++ index.26e203b1.js
@@ -0,0 +1,2 @@
+var player=document.querySelector(".audio");document.querySelector(".cta").addEventListener("click",(function(){player.play(),document.querySelector(".sun").classList.toggle("is-hidden"),document.querySelector(".face").classList.toggle("is-hidden"),document.querySelector(".cat").classList.toggle("is-hidden"),document.querySelector(".me").classList.toggle("is-hidden"),document.querySelector(".heart").classList.toggle("is-hidden"),document.querySelector(".phrase").classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.26e203b1.js.map
--- index.a8089a84.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".cta").addEventListener("click",(function(){document.querySelector(".sun").classList.toggle("is-hidden"),document.querySelector(".face").classList.toggle("is-hidden"),document.querySelector(".cat").classList.toggle("is-hidden"),document.querySelector(".me").classList.toggle("is-hidden"),document.querySelector(".heart").classList.toggle("is-hidden"),document.querySelector(".phrase").classList.toggle("is-hidden")}));
-//# sourceMappingURL=index.a8089a84.js.map
